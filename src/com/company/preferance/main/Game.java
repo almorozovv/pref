2160a90935acce0c60939082955945e19e41a477
Aleksandr Morozov <almorozovv@gmail.com>
2018-03-08 18:38:39 +0400

    add talon (TODO)

--- src/com/company/preferance/main/Game.java
+++ src/com/company/preferance/main/Game.java
@@ -4,14 +4,18 @@ public class Game {
 
     public static void main(String[] args) {
 
+        // создаем игроков
         Player bot1 = new Player("Sam");
         Player bot2 = new Player("Bob");
         Player bot3 = new Player("Mike");
         Dealer bot4 = new Dealer("Joe");
 
-        Card Trump = new Card();
-
+        // создаем козырную карту
+        Card Trump = new Card();  //
+        // узнаем какая масть козырная
         System.out.println("Trump = " + Trump.getSuit());
+        // прикуп
+        //Hand talon = new Hand();  // TO-DO
 
         Hand bot1Hand = new Hand(bot1, 2);
         Hand bot2Hand = new Hand(bot2, 2);
